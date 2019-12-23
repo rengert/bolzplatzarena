b747f084aba319b8483e0a8a7c99e7dfb5969ce4
Thomas Renger <thomas.renger@gmail.com>
2019-12-23 12:05:24 +0100

    feat(ionic): adds ionic to the project

--- apps/projects/nonogramm/src/app/app.component.ts
+++ apps/projects/nonogramm/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
