85b5c79a89bb093d47930b2573e5c31901164788
rengert <thomas.renger@gmail.com>
2020-02-29 17:59:13 +0100

    setup node builds (#18)
    Setup CI workflow

--- apps/projects/core/src/lib/modules/privacy/privacy.module.ts
+++ apps/projects/core/src/lib/modules/privacy/privacy.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '../material/material.module';
+import { MatCardModule } from '@angular/material/card';
 import { ConsentComponent } from './components/consent/consent.component';
 
 @NgModule({
   declarations: [ConsentComponent],
   imports: [
     CommonModule,
-    MaterialModule,
+    MatCardModule,
   ],
 })
 export class PrivacyModule {
--- apps/projects/startup/src/app/modules/dash-board/dash-board.module.ts
+++ apps/projects/startup/src/app/modules/dash-board/dash-board.module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CoreModule } from '../../../../../core/src/lib/core.module';
 import { MaterialModule } from '../../../../../core/src/lib/modules/material/material.module';
 import { DashBoardComponent } from './components/dash-board/dash-board.component';
 
 @NgModule({
-  declarations: [DashBoardComponent],
   imports: [
     CommonModule,
+    CoreModule,
     MaterialModule,
   ],
+  declarations: [DashBoardComponent],
 })
 export class DashBoardModule {
 }
