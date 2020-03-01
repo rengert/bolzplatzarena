b0d3d18bb6faab7b8abfc8012b8ad962d82e3e03
Thomas Renger <thomas.renger@gmail.com>
2020-03-01 17:20:05 +0100

    show simple information in the navigation about the startup and the founder

--- apps/projects/startup/src/app/services/storage/startup-storage.service.ts
+++ apps/projects/startup/src/app/services/storage/startup-storage.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Startup } from '../../models/startup.model';
 
 @Injectable({ providedIn: 'root' })
 export class StartupStorageService {
+  private readonly notify$ = new BehaviorSubject<boolean>(true);
+
   async launched(): Promise<boolean> {
     return new Promise<boolean>(resolve => {
       setTimeout(() => {
@@ -12,11 +16,25 @@ export class StartupStorageService {
     });
   }
 
+  get$(): Observable<Startup | undefined> {
+    return this.notify$.pipe(
+      map(_ => {
+        const data = localStorage.getItem('startup');
+        if (data === null) {
+          return undefined;
+        }
+
+        return JSON.parse(data) as Startup;
+      }),
+    );
+  }
+
   async save(startup: Startup): Promise<boolean> {
     return new Promise<boolean>(resolve => {
       setTimeout(() => {
         const data = JSON.stringify(startup);
         localStorage.setItem('startup', data);
+        this.notify$.next(true);
         resolve(true);
       });
     });
