98d857acc8f77333a69d14f80acc410f6c352420
Thomas Renger <thomas.renger@gmail.com>
2020-03-13 22:28:57 +0100

    add some more random

--- apps/projects/startup/src/app/modules/employee/services/employee.service.ts
+++ apps/projects/startup/src/app/modules/employee/services/employee.service.ts
@@ -1,7 +1,44 @@
 import { Injectable } from '@angular/core';
-import { v4 as uuid } from 'uuid';
+import { sample } from 'lodash';
+import moment, { Moment } from 'moment';
+import { createUuid, randomMoment } from '../../../../../../core/src/lib/utils/common.util';
+import { positions } from '../../../libraries/positions';
+import { Position } from '../../../models/position.model';
 import { Level, Worker } from '../../../models/worker.model';
 import { AppStorageService } from '../../../services/storage/app-storage.service';
+
+function getRandomPosition(): Position {
+  return positions[Math.floor(Math.random() * positions.length)];
+}
+
+function getRandomLevel(): Level {
+  return sample(Object.values(Level)) as Level;
+}
+
+function getRandomBirthday(): Moment {
+  return randomMoment('1970-01-01', '2000-01-01');
+}
+
+function getRandomSalary(position: Position, level: Level, birthday: moment.Moment): number {
+  return Math.floor(Math.random() * 100000);
+}
+
+function getRandomDomicile(): string {
+  return 'Berlin';
+}
+
+function getRandomNames(): { firstname: string, lastname: string } {
+  return {
+    firstname: 'Bob',
+    lastname: 'Last',
+  };
+}
+
+function getRandomPercentage(): number {
+  const percentages = [40, 50, 60, 70, 80, 90, 100, 100, 100, 100, 100, 100, 100, 100, 100];
+
+  return percentages[Math.floor(Math.random() * percentages.length)];
+}
 
 @Injectable({ providedIn: 'root' })
 export class EmployeeService {
@@ -9,45 +46,44 @@ export class EmployeeService {
   }
 
   async seed(): Promise<string> {
-    const data: Worker[] = [
-      {
-        id: uuid(),
-        firstname: 'Greg',
-        lastname: 'Smith',
-        salary: 1,
-        position: { id: uuid(), name: 'Entwickler' },
-        level: Level.junior,
-        domicile: 'Chemnitz',
-        birthday: '1999-01-01',
-        distance: 1000,
-        percentage: 100,
-      },
-      {
-        id: uuid(),
-        firstname: 'Betty',
-        lastname: 'Muller',
-        salary: 10,
-        position: { id: uuid(), name: 'Entwickler' },
-        level: Level.senior,
-        domicile: 'Dresden',
-        birthday: '1989-01-01',
-        distance: 100,
-        percentage: 100,
-      },
-      {
-        id: uuid(),
-        firstname: 'Greg',
-        lastname: 'Smith',
-        salary: 123,
-        position: { id: uuid(), name: 'Entwickler' },
-        level: Level.junior,
-        domicile: 'Berlin',
-        birthday: '1991-01-01',
-        distance: 100,
-        percentage: 80,
-      },
-    ];
+    const data: Worker[] = [];
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
+    data.push(this.random());
 
     return this.appStorage.workers.bulkAdd(data);
+  }
+
+  random(): Worker {
+    const position: Position = getRandomPosition();
+    const level: Level = getRandomLevel();
+    const birthday: Moment = getRandomBirthday();
+    const salary: number = getRandomSalary(position, level, birthday);
+    const domicile: string = getRandomDomicile();
+    const { firstname, lastname } = getRandomNames();
+    const percentage: number = getRandomPercentage();
+    const distance = Math.floor(Math.random() * 1000);
+
+    return {
+      id: createUuid(),
+      firstname,
+      lastname,
+      salary,
+      level,
+      position,
+      domicile,
+      distance,
+      percentage,
+      birthday: birthday.format(),
+    };
   }
 }
