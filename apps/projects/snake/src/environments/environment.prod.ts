551ac0566f17a151809980018e4adfa782ea639b
Thomas Renger <thomas.renger@gmail.com>
2020-03-26 20:00:40 +0100

    fix prod build

--- apps/projects/snake/src/environments/environment.prod.ts
+++ apps/projects/snake/src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '<your-key>',
-    authDomain: '<your-project-authdomain>',
-    databaseURL: '<your-database-URL>',
-    projectId: '<your-project-id>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-messaging-sender-id>',
+    apiKey: 'AIzaSyAXcaeLyAz4_EBFdPKGJNjle15e_aH8e2Y',
+    authDomain: 'bolzplatzarena.firebaseapp.com',
+    databaseURL: 'https://bolzplatzarena.firebaseio.com',
+    projectId: 'bolzplatzarena',
+    storageBucket: 'bolzplatzarena.appspot.com',
+    messagingSenderId: '467927894228',
+    appId: '1:467927894228:web:8e76140c09295982559341',
   },
 };
