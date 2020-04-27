8d2e60dd1640819f8f153de73fc5a25370486f42
Thomas Renger <thomas.renger@gmail.com>
2020-04-27 21:27:35 +0200

    privacy

--- apps/projects/snake3d/src/app/app-routing.module.ts
+++ apps/projects/snake3d/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
 import { SettingsComponent } from './components/settings/settings.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SettingsComponent,
+  },
+  {
+    path: 'privacy',
+    component: PrivacyComponent,
   },
   {
     path: 'snake',
