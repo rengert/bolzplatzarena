a98f4eda85af0e112945869008d3effa5a66e51d
Thomas Renger <thomas.renger@gmail.com>
2020-05-08 14:10:16 +0200

    add the version stuff

--- apps/projects/ultradice/src/app/modules/info/info.module.ts
+++ apps/projects/ultradice/src/app/modules/info/info.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { MaterialModule } from '../../../../../core/src/lib/modules/material/material.module';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
 import { ImprintComponent } from './components/imprint/imprint.component';
 import { PrivacyComponent } from './components/privacy/privacy.component';
+import { VersionComponent } from './components/version/version.component';
 
 @NgModule({
   imports: [
@@ -13,8 +15,9 @@ import { PrivacyComponent } from './components/privacy/privacy.component';
     SharedModule,
     RouterModule,
     MaterialModule,
+    TranslateModule,
   ],
-  declarations: [HomeComponent, ImprintComponent, PrivacyComponent],
+  declarations: [HomeComponent, ImprintComponent, PrivacyComponent, VersionComponent],
 })
 export class InfoModule {
 }
---
+++ apps/projects/ultradice/src/app/version.ts
@@ -0,0 +1,7 @@
+// https://de.wikipedia.org/wiki/Liste_von_Bergen_nach_Schartenh%C3%B6he
+export const version = {
+  value: '1.0.1',
+  name: 'Aconcagua',
+  description: 'Der Aconcagua (mit komplettem Namen: Cerro Aconcagua) ist mit 6961 m[1] der höchste Berg Südamerikas und des amerikanischen Doppelkontinents sowie der höchste Berg außerhalb Asiens und auf der Südhalbkugel. Zugleich ist er der Berg mit der weitesten Dominanz von 16.536 Kilometern (nach dem Mount Everest mit unbezifferter Dominanz) und weist die zweitgrößte Schartenhöhe auf.',
+  slug: 'Aconcagua',
+};
