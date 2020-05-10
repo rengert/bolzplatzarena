860e46a1bc58cf62ca7677f991530e2523c6e6fa
Thomas Renger <thomas.renger@gmail.com>
2020-05-10 21:42:32 +0200

    play beautiful sound

--- apps/projects/neinometer/src/app/app.component.ts
+++ apps/projects/neinometer/src/app/app.component.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
 
+interface NoMessage {
+  id: string;
+  text: string;
+  file: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  semaphore = false;
+
+  data: NoMessage[] = [
+    {
+      id: '1',
+      text: ':)',
+      file: 'nein.doch',
+    },
+    {
+      id: '2',
+      text: ':(',
+      file: 'nein.doch',
+    },
+    {
+      id: '3',
+      text: '=)',
+      file: 'nein.doch',
+    },
+  ];
+
+  async sayNo(message: NoMessage): Promise<void> {
+    if (this.semaphore) {
+      return;
+    }
+
+    this.semaphore = true;
+    const audio = new Audio(`./assets/${message.file}.mp3`);
+    audio.addEventListener('ended', () => {
+      this.semaphore = false;
+    });
+    await audio.play();
+  }
 }
