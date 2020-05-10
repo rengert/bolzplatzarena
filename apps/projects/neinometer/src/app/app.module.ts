48f266361176eb9a1390411ffb05d4100ca2d789
Thomas Renger <thomas.renger@gmail.com>
2020-05-10 22:24:02 +0200

    style & refactor the application

--- apps/projects/neinometer/src/app/app.module.ts
+++ apps/projects/neinometer/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '../../../core/src/lib/modules/material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
+import { SoundsComponent } from './sounds/sounds.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    InfoComponent,
+    SoundsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
