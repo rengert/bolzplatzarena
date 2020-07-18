9fc89e8fe1384af3ddf24fa20b14a074c4e3780d
Thomas Renger <thomas.renger@gmail.com>
2020-07-18 14:40:13 +0200

    use push

--- apps/projects/startup/src/app/app.component.ts
+++ apps/projects/startup/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LoggerService } from '@bpa/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   constructor(private readonly logger: LoggerService<AppComponent>) {
--- apps/projects/startup/src/app/modules/dash-board/components/dash-board/dash-board.component.ts
+++ apps/projects/startup/src/app/modules/dash-board/components/dash-board/dash-board.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
   styleUrls: ['./dash-board.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashBoardComponent {
 }
