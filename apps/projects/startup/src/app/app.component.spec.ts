0480b3a58aafbc1893e4e37d29765d2b2cbda902
Thomas Renger <thomas.renger@gmail.com>
2020-07-26 12:53:09 +0200

    update unit test for the app component

--- apps/projects/startup/src/app/app.component.spec.ts
+++ apps/projects/startup/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
-import { LoggerService } from '@bpa/core';
+import { LoggerService, NotificationService, TitleBarService } from '@bpa/core';
 import { instance, mock } from 'ts-mockito';
-import { TitleBarService } from '../../../core/src/lib/modules/navigation/services/title-bar.service';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     const titleBarMock = mock(TitleBarService);
-    const loggerService = mock(LoggerService);
+    const loggerServiceMock = mock(LoggerService);
+    const notificationServiceMock = mock(NotificationService);
     void TestBed.configureTestingModule({
       imports: [],
       declarations: [
@@ -16,7 +16,8 @@ describe('AppComponent', () => {
       ],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
-        { provide: LoggerService, useFactory: () => instance(loggerService) },
+        { provide: LoggerService, useFactory: () => instance(loggerServiceMock) },
+        { provide: NotificationService, useFactory: () => instance(loggerServiceMock) },
         { provide: TitleBarService, useFactory: () => instance(titleBarMock) },
       ],
     })
