36afebf8c58fd621a3a9e43b5008443c8c737fd8
Thomas Renger <thomas.renger@gmail.com>
2020-07-31 10:53:49 +0200

    implement the basic credit audit

--- apps/projects/startup/src/app/services/credit.service.ts
+++ apps/projects/startup/src/app/services/credit.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
 import { StartupService } from './startup.service';
+import { CreditStorageService } from './storage/credit-storage.service';
 
 @Injectable({ providedIn: 'root' })
 export class CreditService {
-  constructor(private readonly startup: StartupService) {
+  constructor(
+    private readonly startup: StartupService,
+    private readonly creditStorage: CreditStorageService,
+  ) {
   }
 
-  async change(value: number): Promise<void> {
+  async change(value: number, reason?: string): Promise<void> {
     const startUp = await this.startup.get$();
     startUp.credit += value;
     await this.startup.update(startUp)
       .toPromise();
+
+    if (reason) {
+      await this.audit(value, reason);
+    }
+  }
+
+  async audit(value: number, reason: string): Promise<void> {
+    await this.creditStorage.addAudit(value, reason);
   }
 }
