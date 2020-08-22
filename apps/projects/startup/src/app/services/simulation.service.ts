22339633001bca20ca3bb966c0ac47f2e129e02f
Thomas Renger <thomas.renger@gmail.com>
2020-08-22 16:00:29 +0200

    bind the launch to the service

--- apps/projects/startup/src/app/services/simulation.service.ts
+++ apps/projects/startup/src/app/services/simulation.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { createMoment } from '@bpa/core';
 import { Moment } from 'moment';
 import { BehaviorSubject, Observable, of, timer } from 'rxjs';
-import { filter, map, switchMap, tap } from 'rxjs/operators';
+import { filter, first, map, switchMap, tap } from 'rxjs/operators';
+import { StartupService } from './startup.service';
 import { StartupStorageService } from './storage/startup-storage.service';
 
 @Injectable({ providedIn: 'root' })
@@ -13,26 +14,34 @@ export class SimulationService {
   // runs every xxx ms
   private readonly speed = 1000;
 
-  constructor(private readonly startupStorage: StartupStorageService) {
+  constructor(
+    startupSe: StartupService,
+    startupStorage: StartupStorageService,
+  ) {
     this.date$ = this.date.pipe(
       filter(value => !!value),
       map(data => data !),
     );
-    timer(0, this.speed).pipe(
-      map(_ => this.date.value),
-      map(data => data),
-      switchMap(date => {
-        if (!date) {
-          return this.startupStorage.getMoment('simulationDate')
-            .then(value => value || createMoment('2000-01-01'));
-        }
 
-        return of(date);
-      }),
-      map(date => date.clone().add(1, 'hours')),
-      // todo do not use the speeeder .... calculate using speeder
-      tap(date => this.date.next(date)),
-      switchMap(date => this.startupStorage.setMoment('simulationDate', date)),
+    startupSe.launched$().pipe(
+      filter(data => data),
+      first(),
+      switchMap(_ => timer(0, this.speed).pipe(
+        map(__ => this.date.value),
+        map(data => data),
+        switchMap(date => {
+          if (!date) {
+            return startupStorage.getMoment('simulationDate')
+              .then(value => value || createMoment('2000-01-01'));
+          }
+
+          return of(date);
+        }),
+        map(date => date.clone().add(1, 'hours')),
+        // todo do not use the speeeder .... calculate using speeder
+        tap(date => this.date.next(date)),
+        switchMap(date => startupStorage.setMoment('simulationDate', date)),
+      )),
     ).subscribe();
   }
 }
