80598c8cb31856b797f6be941d4c8c32bd6beb93
Thomas Renger <thomas.renger@gmail.com>
2020-08-24 21:05:50 +0200

    extract to separate service

--- apps/projects/startup/src/app/services/simulation.service.ts
+++ apps/projects/startup/src/app/services/simulation.service.ts
@@ -1,52 +1,17 @@
 import { Inject, Injectable } from '@angular/core';
-import { createMoment } from '@bpa/core';
 import { Moment } from 'moment';
-import { BehaviorSubject, Observable, of, timer } from 'rxjs';
-import { filter, first, map, switchMap, tap } from 'rxjs/operators';
 import { SIMULATOR, Simulator } from './simulators/simulator';
+import { TimeSimulatorService } from './simulators/time-simulator.service';
 import { StartupService } from './startup.service';
-import { StartupStorageService } from './storage/startup-storage.service';
 
 @Injectable({ providedIn: 'root' })
 export class SimulationService {
-  readonly date$: Observable<Moment>;
-  private readonly date = new BehaviorSubject<Moment | undefined>(undefined);
-
-  // runs every xxx ms
-  private readonly speed = 10;
-
   constructor(
     startup: StartupService,
-    startupStorage: StartupStorageService,
+    timeSimulator: TimeSimulatorService,
     @Inject(SIMULATOR) private readonly simulators: Simulator[] = [],
   ) {
-    this.date$ = this.date.pipe(
-      filter(value => !!value),
-      map(data => data !),
-    );
-
-    startup.launched$().pipe(
-      filter(data => data),
-      first(),
-      switchMap(_ => timer(0, this.speed).pipe(
-        map(__ => this.date.value),
-        map(data => data),
-        switchMap(date => {
-          if (!date) {
-            return startupStorage.getMoment('simulationDate')
-              .then(value => value || createMoment('2000-01-01'));
-          }
-
-          return of(date);
-        }),
-        map(date => date.clone().add(1, 'hours')),
-        // todo do not use the speeeder .... calculate using speeder
-        tap(date => this.date.next(date)),
-        switchMap(date => startupStorage.setMoment('simulationDate', date)),
-      )),
-    ).subscribe();
-
-    this.date$.subscribe(date => this.handleCosts(date.clone()));
+    timeSimulator.date$.subscribe(date => this.handleCosts(date.clone()));
   }
 
   handleCosts(date: Moment): void {
