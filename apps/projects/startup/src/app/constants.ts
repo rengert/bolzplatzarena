818ec1e08aa9ad8195d804fe36ae9f8d9543355c
Thomas Renger <thomas.renger@gmail.com>
2020-08-29 08:29:21 +0200

    move magic numbers to config

--- apps/projects/startup/src/app/constants.ts
+++ apps/projects/startup/src/app/constants.ts
@@ -1,8 +1,13 @@
+// noinspection MagicNumberJS
 export const CONSTANTS = {
   office: {
     prices: {
       upgrade: [0, 10000, 20000, 50000, 100000],
     },
     space: [5, 10, 15, 22, 50],
+  },
+  payment: {
+    bonus: 1000,
+    christmas: 1500,
   },
 };
--- apps/projects/startup/src/app/services/simulators/salary-simulator.service.ts
+++ apps/projects/startup/src/app/services/simulators/salary-simulator.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { LoggerService } from '@bpa/core';
 import { Moment } from 'moment';
+import { CONSTANTS } from '../../constants';
 import { EmployeeStorageService } from '../../modules/employee/services/storage/employee-storage.service';
 import { CreditService } from '../credit.service';
 import { Simulator } from './simulator';
@@ -37,7 +38,7 @@ export class SalarySimulatorService implements Simulator {
 
     await this.employeeStorage.getEmployed().then(
       async workers => workers.forEach(
-        worker => this.credit.change(-worker.salary, `Monatliches Gehalt ${worker.firstname} ${worker.lastname}`, date)),
+        worker => this.credit.substract(worker.salary, `Monatliches Gehalt ${worker.firstname} ${worker.lastname}`, date)),
     );
   }
 
@@ -50,8 +51,8 @@ export class SalarySimulatorService implements Simulator {
     await this.employeeStorage.getEmployed$().toPromise().then(
       async workers => workers.forEach(
         worker => {
-          this.credit.change(-1000, `Weihnachtsgeld ${worker.firstname} ${worker.lastname}`, date);
-          this.credit.change(-1500, `Bonus ${worker.firstname} ${worker.lastname}`, date);
+          this.credit.substract(CONSTANTS.payment.christmas, `Weihnachtsgeld ${worker.firstname} ${worker.lastname}`, date);
+          this.credit.substract(CONSTANTS.payment.bonus, `Bonus ${worker.firstname} ${worker.lastname}`, date);
         }),
     );
   }
