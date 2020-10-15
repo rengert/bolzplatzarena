e01b7208ad92250e13895bfd226854ac3f5de59e
Thomas Renger <thomas.renger@gmail.com>
2020-10-15 21:55:27 +0200

    feat: enable debugging

--- apps/projects/ascii-racer/src/main.ts
+++ apps/projects/ascii-racer/src/main.ts
@@ -1,4 +1,5 @@
-import { enableProdMode } from '@angular/core';
+import { ApplicationRef, enableProdMode } from '@angular/core';
+import { enableDebugTools } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +9,14 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { ngZoneEventCoalescing: true })
+  .then(moduleRef => {
+    if (!environment.production) {
+      const applicationRef = moduleRef.injector.get(ApplicationRef);
+      const appComponent = applicationRef.components[0];
+      enableDebugTools(appComponent);
+      // tslint:disable-next-line:no-console
+      console.log('debugging is on ...');
+    }
+  })
   .catch(err => console.error(err));
