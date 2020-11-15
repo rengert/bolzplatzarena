a48d110f6592b00b642733c7fb7cb454de0aa616
Thomas Renger <thomas.renger@gmail.com>
2020-11-15 10:08:07 +0100

    fix test startup

--- apps/projects/startup/src/app/app.component.spec.ts
+++ apps/projects/startup/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { LoggerService, NotificationService, TitleBarService } from '@bpa/core';
+import { WorkerManager } from 'angular-web-worker/angular';
 import { instance, mock } from 'ts-mockito';
 import { AppComponent } from './app.component';
 import { SIMULATOR } from './services/simulators/simulator';
@@ -10,6 +11,8 @@ describe('AppComponent', () => {
     const titleBarMock = mock(TitleBarService);
     const loggerServiceMock = mock(LoggerService);
     const notificationServiceMock = mock(NotificationService);
+    const workerManagerMock = mock(WorkerManager);
+
     void TestBed.configureTestingModule({
       imports: [],
       declarations: [
@@ -18,10 +21,10 @@ describe('AppComponent', () => {
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
         { provide: LoggerService, useFactory: () => instance(loggerServiceMock) },
-        { provide: NotificationService, useFactory: () => instance(loggerServiceMock) },
+        { provide: NotificationService, useFactory: () => instance(notificationServiceMock) },
         { provide: TitleBarService, useFactory: () => instance(titleBarMock) },
+        { provide: WorkerManager, useFactory: () => instance(workerManagerMock) },
         { provide: SIMULATOR, useValue: [], multi: true },
-
       ],
     })
       .compileComponents();
