72b1faddfec8d52e6bc8644aa60ed774ae75fe73
Thomas Renger <thomas.renger@gmail.com>
2020-12-07 11:24:03 +0100

    styling header & notes

--- apps/projects/startup/src/app/components/content/notes/notes.component.ts
+++ apps/projects/startup/src/app/components/content/notes/notes.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-notes',
@@ -7,4 +7,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotesComponent {
+  @HostBinding('class') class = 'full-size';
 }
