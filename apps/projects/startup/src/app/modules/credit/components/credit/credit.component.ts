6ed5cd9d04076588e0c1a76e325d960b901ec5c4
Thomas Renger <thomas.renger@gmail.com>
2020-12-12 17:45:40 +0100

    show potential credits

--- apps/projects/startup/src/app/modules/credit/components/credit/credit.component.ts
+++ apps/projects/startup/src/app/modules/credit/components/credit/credit.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Credit } from '../../models/credit.model';
+import { CreditService } from '../../services/credit.service';
 
 @Component({
   selector: 'app-credit',
@@ -7,4 +10,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreditComponent {
+  readonly myCredits$: Observable<Credit[]>;
+  readonly availableCredits$: Observable<Credit[]>;
+
+  constructor(private readonly credit: CreditService) {
+    this.myCredits$ = this.credit.list$();
+    this.availableCredits$ = this.credit.generate$();
+  }
+
+  action(credit: Credit): void {
+    this.credit.borrow(credit, credit.originalAmount);
+  }
 }
--- apps/projects/startup/src/app/modules/credit/services/credit.service.ts
+++ apps/projects/startup/src/app/modules/credit/services/credit.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Credit } from '../models/credit.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CreditService {
   constructor() {
   }
 
-  list(): Credit[] {
-    return [];
+  list$(): Observable<Credit[]> {
+    return of([]);
+  }
+
+  generate$(): Observable<Credit[]> {
+    const result: Credit[] = [{
+      name: 'Ergo Bank',
+      amount: 100000,
+      costPerMonth: 10000,
+      interestRate: 0.0125,
+      months: 12,
+      originalAmount: 100000,
+    }];
+    return of(result);
   }
 
   repay(credit: Credit, value: number): void {
