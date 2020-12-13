3968c61d7a5e80eec7204330c0f5c9806af6c811
Thomas Renger <thomas.renger@gmail.com>
2020-12-13 17:21:11 +0100

    refresh the credit list to update after borrow / repay

--- apps/projects/startup/src/app/modules/credit/components/credit/credit.component.ts
+++ apps/projects/startup/src/app/modules/credit/components/credit/credit.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Credit } from '../../models/credit.model';
 import { CreditService } from '../../services/credit.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-credit',
@@ -13,17 +14,21 @@ export class CreditComponent {
   readonly myCredits$: Observable<Credit[]>;
   readonly availableCredits$: Observable<Credit[]>;
 
+  private readonly refresh = new BehaviorSubject(undefined);
+
   constructor(private readonly credit: CreditService) {
     this.myCredits$ = this.credit.list$();
-    this.availableCredits$ = this.credit.generate$();
+    this.availableCredits$ = this.refresh.pipe(switchMap(() => this.credit.generate$()));
   }
 
   action(credit: Credit): void {
     this.credit.borrow(credit);
+    this.refresh.next(undefined);
   }
 
   repay(credit: Credit): void {
     // todo: nur teilweise zurückzahlen
     this.credit.repay(credit, credit.amount);
+    this.refresh.next(undefined);
   }
 }
