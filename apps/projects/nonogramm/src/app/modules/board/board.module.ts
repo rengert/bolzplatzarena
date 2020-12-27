01c0bce634f8416cafbbfed5cb31696de15d175d
Thomas Renger <thomas.renger@gmail.com>
2020-12-27 11:18:26 +0100

    beautify code

--- apps/projects/nonogramm/src/app/modules/board/board.module.ts
+++ apps/projects/nonogramm/src/app/modules/board/board.module.ts
@@ -7,12 +7,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { TranslateModule } from '@ngx-translate/core';
-import { CoreModule } from '../../../../../core/src/lib/core.module';
 import { BlockComponent } from './components/board/block/block.component';
 import { BoardComponent } from './components/board/board.component';
 import { CaptionComponent } from './components/board/caption/caption.component';
 import { HeartsComponent } from './components/board/hearts/hearts.component';
 import { SettingsViewComponent } from './components/board/settings-view/settings-view.component';
+import { CoreModule } from '@bpa/core';
 
 @NgModule({
   declarations: [
