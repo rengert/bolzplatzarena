6e94bd72270d3259818fa37d88958d62e1e754b2
Thomas Renger <thomas.renger@gmail.com>
2020-12-29 14:58:10 +0100

    simple enemies using the path finding

--- apps/projects/tower-defense/src/app/modules/tower-defense/services/tower-defense.service.ts
+++ apps/projects/tower-defense/src/app/modules/tower-defense/services/tower-defense.service.ts
@@ -1,6 +1,6 @@
 import { ElementRef, Injectable } from '@angular/core';
 import { EngineService } from './engine.service';
-import { ActionManager, Color3, ExecuteCodeAction, Mesh, Scene, StandardMaterial } from '@babylonjs/core';
+import { ActionManager, Color3, ExecuteCodeAction, Mesh, MeshBuilder, Scene, StandardMaterial, Vector3 } from '@babylonjs/core';
 import { createUuid } from '@bpa/core';
 import { AStarFinder } from 'astar-typescript';
 
@@ -14,34 +14,58 @@ interface Field {
   mesh: Mesh;
 }
 
-@Injectable({
-  providedIn: 'root',
-})
+interface Enemy {
+  energy: number;
+  mesh: Mesh;
+  target?: Coordinate;
+  source: Coordinate;
+}
+
+@Injectable({ providedIn: 'root' })
 export class TowerDefenseService {
-  fields: Field[][];
+  private fields: Field[][];
 
-  material: StandardMaterial;
-  blockedMaterial: StandardMaterial;
-  hoverMaterial: StandardMaterial;
+  private enemyMaterial: StandardMaterial;
+  private material: StandardMaterial;
+  private blockedMaterial: StandardMaterial;
+  private hoverMaterial: StandardMaterial;
 
-  readonly start: Coordinate = { x: 0, y: 0 };
-  readonly end: Coordinate = { x: 19, y: 9 };
+  private readonly start: Coordinate = { x: 0, y: 0 };
+  private readonly end: Coordinate = { x: 19, y: 9 };
+  private readonly size = { width: 10, height: 20 };
 
-  path: Coordinate[];
+  private path: Coordinate[];
+
+  private enemies: Enemy[] = [];
 
   constructor(private readonly engine: EngineService) {
   }
 
   init(canvas: ElementRef<HTMLCanvasElement>): void {
-    const size = { width: 10, height: 20 };
-    const scene = this.initScene(canvas, size);
-    this.initPlayground(scene, size);
+    const scene = this.initScene(canvas, this.size);
+    this.initPlayground(scene, this.size);
     const path = this.pathFinding();
     this.highlightPath(path);
   }
 
   private initScene(canvas: ElementRef<HTMLCanvasElement>, size: { width: number, height: number }): Scene {
     const scene = this.engine.createScene(canvas.nativeElement, size);
+
+    this.material = new StandardMaterial('StandardMaterial', this.engine.scene);
+    this.material.alpha = 1;
+    this.material.diffuseColor = new Color3(0.123, 0.456, 0.789);
+
+    this.hoverMaterial = new StandardMaterial('StandardMaterial', this.engine.scene);
+    this.hoverMaterial.alpha = 1;
+    this.hoverMaterial.diffuseColor = new Color3(0.123, 1, 0.789);
+
+    this.blockedMaterial = new StandardMaterial('StandardMaterial', this.engine.scene);
+    this.blockedMaterial.alpha = 1;
+    this.blockedMaterial.diffuseColor = new Color3(0.823, 0.1, 0.789);
+
+    this.enemyMaterial = new StandardMaterial('Enemy', this.engine.scene);
+    this.enemyMaterial.alpha = 1;
+    this.enemyMaterial.diffuseColor = new Color3(0.99, 0, 0);
 
     scene.registerBeforeRender(() => {
       this.beforeRender();
@@ -56,23 +80,11 @@ export class TowerDefenseService {
   }
 
   private initPlayground(scene: Scene, size: { width: number, height: number }): void {
-    this.material = new StandardMaterial('StandardMaterial', this.engine.scene);
-    this.material.alpha = 1;
-    this.material.diffuseColor = new Color3(0.123, 0.456, 0.789);
-
-    this.hoverMaterial = new StandardMaterial('StandardMaterial', this.engine.scene);
-    this.hoverMaterial.alpha = 1;
-    this.hoverMaterial.diffuseColor = new Color3(0.123, 1, 0.789);
-
-    this.blockedMaterial = new StandardMaterial('StandardMaterial', this.engine.scene);
-    this.blockedMaterial.alpha = 1;
-    this.blockedMaterial.diffuseColor = new Color3(0.823, 0.1, 0.789);
-
     this.fields = [];
     for (let i = 0; i < size.width; i++) {
       this.fields[i] = [];
       for (let j = 0; j < size.height; j++) {
-        const mesh = Mesh.CreateSphere(`field-${createUuid()}`, 16, 1, this.engine.scene);
+        const mesh = MeshBuilder.CreateBox(`field-${createUuid()}`, { size: 1 }, this.engine.scene);
         mesh.material = this.material;
         mesh.position.x = i - size.width / 2 + 0.5;
         mesh.position.z = j - size.height / 2 + 0.5;
@@ -117,9 +129,41 @@ export class TowerDefenseService {
     }
   }
 
+  private updateEnemies(path: number[][]): void {
+    for (const enemy of this.enemies) {
+      if (!enemy.target) {
+        const [y, x] = path[path.findIndex(([y, x]: number[]) => x === enemy.source.x && y === enemy.source.y) + 1] !;
+        enemy.target = { x, y };
+      }
+      const target = this.fields[enemy.target.x][enemy.target.y];
+      let deltaTargetSource = target.mesh.position.subtract(this.fields[enemy.source.x][enemy.source.y].mesh.position);
+      enemy.mesh.position.x += deltaTargetSource.x * 0.02;
+      enemy.mesh.position.z += deltaTargetSource.z * 0.02;
+      enemy.mesh.position.y += deltaTargetSource.y * 0.02;
+
+      const enemyDelta = target.mesh.position.subtract(enemy.mesh.position);
+      enemyDelta.y = 0;
+      if (enemyDelta.equalsWithEpsilon(Vector3.Zero())) {
+        enemy.source = enemy.target;
+        enemy.target = undefined;
+      }
+    }
+  }
+
   private beforeRender(): void {
     const path = this.pathFinding();
     this.highlightPath(path);
+
+    if (this.enemies.length < 2000) {
+      const mesh = Mesh.CreateSphere(`enemy-${createUuid()}`, 32, 0.125, this.engine.scene);
+      mesh.material = this.enemyMaterial;
+      mesh.position.x = -this.size.width / 2 + 0.5;
+      mesh.position.z = -this.size.height / 2 + 0.5;
+      mesh.position.y = 1;
+      this.enemies.push({ mesh, energy: 1, source: this.start });
+    }
+
+    this.updateEnemies(path);
   }
 
   private afterRender(): void {
