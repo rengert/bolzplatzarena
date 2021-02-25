b957361ce242a9afad164a8cdf8bbecff176f667
Thomas Renger <thomas.renger@gmail.com>
2021-02-25 17:26:12 +0100

    pwa check

--- apps/projects/nonogramm/src/app/app.component.ts
+++ apps/projects/nonogramm/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate } from '@angular/service-worker';
 import { TranslateService } from '@ngx-translate/core';
 import { Angulartics2Piwik } from 'angulartics2/piwik';
 
@@ -13,15 +15,19 @@ export class AppComponent implements OnInit {
   constructor(
     private readonly tracking: Angulartics2Piwik,
     private readonly translate: TranslateService,
+    private readonly update: SwUpdate,
+    private readonly snackBar: MatSnackBar,
   ) {
     this.tracking.startTracking();
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     const lang = 'de';
     this.translate.use(lang);
     // eslint-disable-next-line @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires
     const data = require(`../i18n/${lang}.json`);
     this.translate.setTranslation(lang, data, true);
+
+    this.update.available.subscribe(_ => alert('update'));
   }
 }
