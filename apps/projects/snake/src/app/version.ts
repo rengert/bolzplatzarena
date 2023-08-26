d27510699a1dcc01488c33f9a6ceba2643ca07b1
Thomas Renger <thomas.renger@gmail.com>
2023-08-26 16:05:10 +0200

    chore: version update

--- apps/projects/snake/src/app/version.ts
+++ apps/projects/snake/src/app/version.ts
@@ -1,5 +1,5 @@
 export const version = {
-  value: '1.0.0',
+  value: '1.0.1',
   name: 'Acanthophis antarcticus',
   // eslint-disable-next-line max-len
   description: 'Die Todesotter (Acanthophis antarcticus, Syn.: Acanthophis schistos) ist eine Schlangenart aus der Familie der Giftnattern (Elapidae), die in weiten Teilen des südlichen, östlichen und nördlichen Australiens vorkommt.',
