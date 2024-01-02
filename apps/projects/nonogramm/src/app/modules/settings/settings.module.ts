ad7737084b821ed3547676e4de2fe13f3e5f68ed
Thomas Renger <thomas.renger@gmail.com>
2024-01-02 16:12:05 +0100

    fix: import path

--- apps/projects/nonogramm/src/app/modules/settings/settings.module.ts
+++ apps/projects/nonogramm/src/app/modules/settings/settings.module.ts
@@ -1,24 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { TranslateModule } from '@ngx-translate/core';
-import { CoreModule } from '../../../../../core/src/lib/core.module';
-import { SettingsComponent } from './components/settings/settings.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRadioModule } from '@angular/material/radio';
+import { CoreModule } from '@bpa/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        CoreModule,
-        FormsModule,
-        MatButtonModule,
-        MatCardModule,
-        MatRadioModule,
-        TranslateModule,
-        SettingsComponent,
-    ],
+  imports: [
+    CommonModule,
+    CoreModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatRadioModule,
+    TranslateModule,
+    SettingsComponent,
+  ],
 })
 export class SettingsModule {
 }
