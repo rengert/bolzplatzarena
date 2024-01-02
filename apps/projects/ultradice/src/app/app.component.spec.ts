ff6e79e94bbc3c766f80436a6339efea62b2dafe
Thomas Renger <thomas.renger@gmail.com>
2024-01-02 23:02:05 +0100

    fix: import for the tests

--- apps/projects/ultradice/src/app/app.component.spec.ts
+++ apps/projects/ultradice/src/app/app.component.spec.ts
@@ -1,15 +1,14 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { LoggerService, NotificationService, TitleBarService } from '@bpa/core';
+import { DialogService, LoggerService, NotificationService, TitleBarService } from '@bpa/core';
 import { TranslateService } from '@ngx-translate/core';
 import { instance, mock } from 'ts-mockito';
 import 'zone.js/testing';
-import { DialogService } from '../../../core/src/lib/modules/dialog/services/dialog.service';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(waitForAsync(async () => {
+  beforeEach(waitForAsync(async() => {
     const titleBarMock = mock(TitleBarService);
     const loggerServiceMock = mock(LoggerService);
     const notificationServiceMock = mock(NotificationService);
@@ -17,19 +16,19 @@ describe('AppComponent', () => {
     const translateService = mock(TranslateService);
 
     await TestBed.configureTestingModule({
-    imports: [
+      imports: [
         RouterTestingModule,
         AppComponent,
-    ],
-    schemas: [NO_ERRORS_SCHEMA],
-    providers: [
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
         { provide: LoggerService, useFactory: () => instance(loggerServiceMock) },
         { provide: NotificationService, useFactory: () => instance(notificationServiceMock) },
         { provide: TitleBarService, useFactory: () => instance(titleBarMock) },
         { provide: DialogService, useFactory: () => instance(dialogServiceMock) },
         { provide: TranslateService, useFactory: () => instance(translateService) },
-    ],
-})
+      ],
+    })
       .compileComponents();
   }));
 
